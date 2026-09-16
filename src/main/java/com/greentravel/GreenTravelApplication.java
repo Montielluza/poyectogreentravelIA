ef6865a9b93a7c12b2f72d1fb0beb2bf3b7de629
Luza Montiel <luzamontiel20@gmail.com>
2026-09-16 03:36:05 -0500

    first commit

---
+++ src/main/java/com/greentravel/GreenTravelApplication.java
@@ -0,0 +1,13 @@
+package com.greentravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenTravelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenTravelApplication.class, args);
+	}
+
+}
